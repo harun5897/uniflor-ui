866e098531a00d26b9e1189b2ea1651242bb2922
trian damai <triannurizkillah@gmail.com>
2025-03-24 21:39:51 +0700

    feat: add HeroAboutUs component with props for breadcrumbs, title, and image

---
+++ components/univ/HeroAboutUs.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+defineProps<{
+    breadcrumbs: Array<string>,
+    title: string,
+    image: string
+}>()
+</script>
+<template>
+    <div
+        id="section-univ-hero-about-us"
+        class="w-screen h-full min-h-1/3 flex flex-row justify-between items-center"
+    >
+        <!-- Breadcrumbs and title -->
+        <div>
+            <span v-for="breadcrumb in breadcrumbs">
+                {{ breadcrumb }}
+            </span>
+            <div>
+                {{ title }}
+            </div>
+        </div>
+        <!-- IMAGE -->
+        <div>
+        
+        </div>
+    </div>
+</template>
