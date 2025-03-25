4352428b54c5c86c25e5cd5410f3a1420ea071ca
trian damai <triannurizkillah@gmail.com>
2025-03-25 09:05:03 +0700

    feat: update ui

--- components/univ/HeroAboutUs.vue
+++ components/univ/HeroAboutUs.vue
@@ -8,21 +8,23 @@ defineProps<{
 <template>
     <div
         id="section-univ-hero-about-us"
-        class="w-screen h-full min-h-1/3 flex flex-row justify-between items-center"
+        class="w-screen h-full min-h-1/3 flex flex-row justify-between items-center bg-primary-light px-8 py-10"
     >
         <!-- Breadcrumbs and title -->
         <div>
-            <span
-                v-for="(breadcrumb, index) in breadcrumbs"
-                class="mr-1 flex flex-row items-center"
-            >
-                {{ breadcrumb }}
-                <Icon
-                    v-show="index < breadcrumbs.length"
-                    name="ic:round-keyboard-arrow-right"
-                />
-            </span>
-            <div>
+            <div class="flex flex-row justify-start items-center">
+                <span
+                    v-for="(breadcrumb, index) in breadcrumbs"
+                    class="h-max flex flex-row items-center"
+                >
+                    {{ breadcrumb }}
+                    <Icon
+                        v-show="index < breadcrumbs.length - 1"
+                        name="ic:round-keyboard-arrow-right"
+                    />
+                </span>
+            </div>
+            <div class="text-2xl font-lato font-semibold text-primary-main">
                 {{ title }}
             </div>
         </div>
