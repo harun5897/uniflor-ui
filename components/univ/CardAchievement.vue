2d2ec1bc0c0f1fc549312af786d91f2fa0a03112
Harun <harungb025@gmail.com>
2025-04-26 17:16:40 +0700

    feat: create component card achievement and achievement

---
+++ components/univ/CardAchievement.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  image: string
+  title: string | null
+  date: string
+}>()
+</script>
+<template>
+  <div class="w-full h-full bg-white rounded-lg shadow">
+    <NuxtLink to="/">
+      <img
+        :src="getPublicUrl(image, 'assets')"
+        class="h-[200px] md:h-[230px] lg:h-[200px] xl:h-[250px] w-full object-cover rounded-t-lg"
+        alt="alt"
+      />
+      <div class="py-6 px-4">
+        <h3 class="text-xl font-lato font-bold mt-2 text-ellipsis overflow-hidden line-clamp-2">
+          {{ title }}
+        </h3>
+        <h6 class="text-sm text-grey-600 font-kumbh-sans font-normal my-3">{{ date }}</h6>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
