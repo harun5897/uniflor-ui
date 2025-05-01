59cef88c1343ab82df69bd97c619bdc4a364d2e7
Harun <harungb025@gmail.com>
2025-05-01 14:19:17 +0700

    refactor: remove style padding

--- components/univ/CardNews.vue
+++ components/univ/CardNews.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
 defineProps<{
-  categories: string
-  banner: string
-  title: string | null
-  date: string
+  categories: Array<NewsCategory>,
+  banner: string,
+  title: string | null,
+  date: string,
 }>()
 </script>
 <template>
-  <div class="w-full h-full bg-white rounded-lg">
+  <div class="w-full h-full bg-white rounded-lg transition-transform duration-300 hover:scale-105">
     <img
       :src="getPublicUrl(banner, 'assets')"
       class="h-[200px] md:h-[230px] lg:h-[200px] xl:h-[250px] w-full object-cover rounded-t-lg"
       alt="alt"
     />
-    <div class="py-6 px-4">
+    <div class="pt-4">
       <div class="inline-block border border-grey-300 rounded-full py-1.5 px-3 mb-2">
         <h6
           v-for="category in categories"
           class="text-xs text-grey-800 font-kumbh-sans font-medium"
         > {{ (category as any).news_category.name }}</h6>
       </div>
-      <h3 class="text-xl font-lato font-bold mt-2 text-ellipsis overflow-hidden line-clamp-2">
+      <h3 class="text-xl font-lato font-bold mt-0.5 text-ellipsis overflow-hidden line-clamp-2">
         {{ title }}
       </h3>
       <h6 class="text-sm text-grey-600 font-kumbh-sans font-normal my-3">{{ date }}</h6>
       <NuxtLink
         to="/"
-        class="text-base font-bold text-grey-800"
+        class="text-base font-bold text-grey-800 hover:text-primary-main"
       >
         Selengkapnya
       </NuxtLink>
